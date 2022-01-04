0e55f910ab856cbba6fd3663e55f175de1f31d9c
U-JOHNLIU\jonhl <johnliu>
2022-01-04 18:26:54 +0800

    add v4.3.10

--- src/emqx_exhook_app.erl
+++ src/emqx_exhook_app.erl
@@ -1,5 +1,5 @@
 %%--------------------------------------------------------------------
-%% Copyright (c) 2020 EMQ Technologies Co., Ltd. All Rights Reserved.
+%% Copyright (c) 2020-2021 EMQ Technologies Co., Ltd. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -14,9 +14,31 @@
 %% limitations under the License.
 %%--------------------------------------------------------------------
 
--ifndef(EMQX_EXTENSION_HOOK_HRL).
--define(EMQX_EXTENSION_HOOK_HRL, true).
+-module(emqx_exhook_app).
 
--define(APP, emqx_extension_hook).
+-behaviour(application).
 
--endif.
+-include("emqx_exhook.hrl").
+
+-emqx_plugin(extension).
+
+-export([ start/2
+        , stop/1
+        , prep_stop/1
+        ]).
+
+%%--------------------------------------------------------------------
+%% Application callbacks
+%%--------------------------------------------------------------------
+
+start(_StartType, _StartArgs) ->
+    {ok, Sup} = emqx_exhook_sup:start_link(),
+    emqx_ctl:register_command(exhook, {emqx_exhook_cli, cli}, []),
+    {ok, Sup}.
+
+prep_stop(State) ->
+    emqx_ctl:unregister_command(exhook),
+    State.
+
+stop(_State) ->
+    ok.
--- src/emqx_exhook_cli.erl
+++ src/emqx_exhook_cli.erl
@@ -1,5 +1,5 @@
 %%--------------------------------------------------------------------
-%% Copyright (c) 2020 EMQ Technologies Co., Ltd. All Rights Reserved.
+%% Copyright (c) 2020-2021 EMQ Technologies Co., Ltd. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -14,44 +14,38 @@
 %% limitations under the License.
 %%--------------------------------------------------------------------
 
--module(emqx_extension_hook_cli).
+-module(emqx_exhook_cli).
 
--include("emqx_extension_hook.hrl").
+-include("emqx_exhook.hrl").
 
 -export([cli/1]).
 
-cli(["drivers", "list"]) ->
+cli(["server", "list"]) ->
     if_enabled(fun() ->
-        Drivers = emqx_extension_hook:list(),
-        [emqx_ctl:print("Driver(~s)~n", [emqx_extension_hook_driver:format(Driver)]) || Driver <- Drivers]
+        ServerNames = emqx_exhook:list(),
+        [emqx_ctl:print("Server(~s)~n", [format(Name)]) || Name <- ServerNames]
     end);
 
-cli(["drivers", "enable", Name0]) ->
+cli(["server", "enable", Name]) ->
     if_enabled(fun() ->
-        Name = list_to_atom(Name0),
-        case proplists:get_value(Name, application:get_env(?APP, drivers, [])) of
-            undefined ->
-                emqx_ctl:print("not_found~n");
-            Opts ->
-                print(emqx_extension_hook:enable(Name, Opts))
-        end
+        print(emqx_exhook:enable(list_to_existing_atom(Name)))
     end);
 
-cli(["drivers", "disable", Name]) ->
+cli(["server", "disable", Name]) ->
     if_enabled(fun() ->
-        print(emqx_extension_hook:disable(list_to_atom(Name)))
+        print(emqx_exhook:disable(list_to_existing_atom(Name)))
     end);
 
-cli(["drivers", "stats"]) ->
+cli(["server", "stats"]) ->
     if_enabled(fun() ->
         [emqx_ctl:print("~-35s:~w~n", [Name, N]) || {Name, N} <- stats()]
     end);
 
 cli(_) ->
-    emqx_ctl:usage([{"exhook drivers list", "List all running drivers"},
-                    {"exhook drivers enable <Name>", "Enable a driver with configurations"},
-                    {"exhook drivers disable <Name>", "Disable a driver"},
-                    {"exhook drivers stats", "Print drivers statistic"}]).
+    emqx_ctl:usage([{"exhook server list", "List all running exhook server"},
+                    {"exhook server enable <Name>", "Enable a exhook server in the configuration"},
+                    {"exhook server disable <Name>", "Disable a exhook server"},
+                    {"exhook server stats", "Print exhook server statistic"}]).
 
 print(ok) ->
     emqx_ctl:print("ok~n");
@@ -64,17 +58,26 @@ print({error, Reason}) ->
 
 if_enabled(Fun) ->
     case lists:keymember(?APP, 1, application:which_applications()) of
-        true -> Fun();
+        true ->
+            Fun();
         _ -> hint()
     end.
 
 hint() ->
-    emqx_ctl:print("Please './bin/emqx_ctl plugins load emqx_extension_hook' first.~n").
+    emqx_ctl:print("Please './bin/emqx_ctl plugins load emqx_exhook' first.~n").
 
 stats() ->
-    lists:foldr(fun({K, N}, Acc) ->
+    lists:usort(lists:foldr(fun({K, N}, Acc) ->
         case atom_to_list(K) of
-            "exhook." ++ Key -> [{Key, N}|Acc];
+            "exhook." ++ Key -> [{Key, N} | Acc];
             _ -> Acc
         end
-    end, [], emqx_metrics:all()).
+    end, [], emqx_metrics:all())).
+
+format(Name) ->
+    case emqx_exhook_mngr:server(Name) of
+        undefined ->
+            io_lib:format("name=~s, hooks=#{}, active=false", [Name]);
+        Server ->
+            emqx_exhook_server:format(Server)
+    end.
